56dbabc95344cf6aa6f8f64b319e2045977367dc
joannac112 <joannac112@nycstudents.net>
2025-05-03 20:08:14 +0000

    save2

--- WordPairList.java
+++ WordPairList.java
@@ -2,12 +2,13 @@ import java.util.ArrayList;
 public class WordPairList {
     /** The list of word pairs, initialized by the constructor. */
     private ArrayList<WordPair> allPairs;
-    allPairs = new ArrayList<WordPair>();
+
     /**
      * Constructs a WordPairList object as described in part (a).
      * Precondition: words.length >= 2
      */
     public WordPairList(String[] words) {
+        allPairs = new ArrayList<WordPair>();
         for (int i = 0; i < words.length -1; i++){
             for (int j = i + 1; j < words.length; j++){
                 WordPair w = new WordPair(words[i], words[j]);
